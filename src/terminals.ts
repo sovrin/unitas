085c079dd528a1c897c4d1d1608b0032e66e10a0
Oleg Kamlowski <kamlowski.oleg@gmail.com>
2026-02-02 17:47:30 +0100

    refactor(): move primitives to terminals
    they're basically the same

--- src/terminals.ts
+++ src/terminals.ts
@@ -1,6 +1,13 @@
 import { failure, success } from './results';
 import { choice, create, surrounded } from './combinators';
 import { Char, Parser } from './types';
+import {
+    Char,
+    Letter,
+    LowerCaseLetter,
+    Parser,
+    UpperCaseLetter,
+} from './types';
 
 export const literal = <S extends string>(str: S) => {
     return create<S>((input) =>
@@ -109,3 +116,37 @@ export const braced = <T>(content: Parser<T>) => {
 export const bracketed = <T>(content: Parser<T>) => {
     return surrounded(literal('['), content, literal(']'));
 };
+
+export function satisfy<T extends string>(predicate: (c: Char<T>) => boolean) {
+    return create<T>((input) => {
+        const c = input[0] as Char<T>;
+        return input.length > 0 && predicate(c)
+            ? success(c as T, input.slice(1))
+            : failure();
+    });
+}
+
+export const digit = map<string, number>(
+    satisfy((c: string) => /^[0-9]$/.test(c)),
+    (c) => parseInt(c, 10),
+);
+
+export const digits = create<number>((input) => {
+    const result = many1(digit)(input);
+    if (!result) {
+        return failure();
+    }
+
+    const [ds, rest] = result;
+    const value = ds.reduce((acc, d) => acc * 10 + d, 0);
+
+    return success(value, rest);
+});
+
+export const letter = satisfy<Letter>((c) => /[a-zA-Z]/.test(c));
+
+export const whitespace = satisfy<' '>((c) => /\s/.test(c));
+
+export const uppercase = satisfy<UpperCaseLetter>((c) => /[A-Z]/.test(c));
+
+export const lowercase = satisfy<LowerCaseLetter>((c) => /[a-z]/.test(c));
