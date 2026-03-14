4479a49f9f8f4ee5a98b070e221336c5c35543e4
Oleg Kamlowski <kamlowski.oleg@gmail.com>
2026-03-14 15:46:40 +0100

    fix(combinators): guard

--- src/combinators/guard.test.ts
+++ src/combinators/guard.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from 'vitest';
+import { describe, it } from 'vitest';
 
 import { assertResult, createTestParser } from '../../test/utils.test';
 import { guard } from './guard';
@@ -12,20 +12,18 @@ describe('guard', () => {
         assertResult<'A' | null>(result, ['A', 'AA']);
     });
 
-    it('should return null and consume no input when condition is false', () => {
+    it('should fail when condition is false', () => {
         const parser1 = createTestParser('A');
         const parser = guard(false, parser1);
         const result = parser('BBB');
-        expect(result).toEqual([null, 'BBB']);
 
-        assertResult<'A' | null>(result, [null, 'BBB']);
+        assertResult<'A' | null>(result);
     });
 
     it('should propagate parser failure when condition is true but parser fails', () => {
         const parser1 = createTestParser('A');
         const parser = guard(true, parser1);
         const result = parser('BBB');
-        expect(result).toBeNull();
 
         assertResult<'A' | null>(result);
     });
--- src/combinators/guard.ts
+++ src/combinators/guard.ts
@@ -1,9 +1,14 @@
-import { create } from '../core/create';
-import { success } from '../core/success';
-import type { Parser } from '../types';
+import {create} from '../core/create';
+import {failure} from '../core/failure';
+import type {Parser} from '../types';
 
 export const guard = <T>(condition: boolean, parser: Parser<T>) => {
     return create<T | null>((input) => {
-        return condition ? parser(input) : success(null, input);
+        if (!condition) {
+            return failure();
+        }
+
+        const result = parser(input);
+        return result ? result : failure();
     });
 };
