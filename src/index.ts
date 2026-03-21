23df6b35162ee32070199f5a752e38aa5f130aab
Oleg Kamlowski <kamlowski.oleg@gmail.com>
2026-03-21 21:54:05 +0100

    fix(): index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-export * from './combinators';
-export * from './terminals';
-export * from './utils';
-export * from './types';
+/**
+ * User: Oleg Kamlowski <oleg.kamlowski@thomann.io>
+ * Date: 21.03.26
+ * Time: 18:06
+ */
 export * from './core';
