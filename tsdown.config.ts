8b008f8e9c69ecdbb541eda93c91ac23a318d2b2
Oleg Kamlowski <kamlowski.oleg@gmail.com>
2026-04-07 13:41:05 +0200

    fix(package): remove cjs support

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,31 +3,31 @@ import { defineConfig } from 'tsdown';
 export default defineConfig([
     {
         entry: { index: './src/index.ts' },
-        format: ['esm', 'cjs'],
+        format: ['esm'],
         minify: true,
         dts: true,
     },
     {
         entry: { combinators: './src/combinators/index.ts' },
-        format: ['esm', 'cjs'],
+        format: ['esm'],
         minify: true,
         dts: true,
     },
     {
         entry: { terminals: './src/terminals/index.ts' },
-        format: ['esm', 'cjs'],
+        format: ['esm'],
         minify: true,
         dts: true,
     },
     {
         entry: { primitives: './src/primitives/index.ts' },
-        format: ['esm', 'cjs'],
+        format: ['esm'],
         minify: true,
         dts: true,
     },
     {
         entry: { utils: './src/utils/index.ts' },
-        format: ['esm', 'cjs'],
+        format: ['esm'],
         minify: true,
         dts: true,
     },
